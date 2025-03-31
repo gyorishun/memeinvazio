bf5cbd4e693d03c0af2deab978d7f314f01c9850
tanulo <tanulo@barossgabor.local>
2025-03-31 13:41:14 +0200

    bejelentkezes befejezese es home letrehot

--- js/login.js
+++ js/login.js
@@ -8,13 +8,35 @@ async function login() {
 
     console.log(email,psw);
 
-    const response = await fetch('http://127.0.0.1:3000/api/auth/login' , {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify({ email, psw }),
-        credentials: 'include' //engedélyezi a sutik fogadasat 
-    });
+    try {
+        const response = await fetch('http://127.0.0.1:3000/api/auth/login' , {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ email, psw }),
+            credentials: 'include' //engedélyezi a sutik fogadasat 
+        });
+        console.log(response);
+        const data = await response.json();
+
+        if (response.ok){
+            alert(data.message);
+            window.location.href = '../html/home.html';
+        } else if (data.errors) {
+            let errorMessage = '';
+            data.errors.forEach(sor => {
+                errorMessage += `${sor.error}\n`;
+            });
+            alert(errorMessage);
+        }else if (data.error){
+            alert(data.error);
+        }else {
+            alert('Ismeretlen hiba')
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
 }
 
