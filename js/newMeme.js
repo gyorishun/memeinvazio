44c7ebaac3b68ef536979c0db7d81ac10ef8633e
tanulo <tanulo@barossgabor.local>
2025-05-22 10:57:03 +0200

    valami

--- js/newMeme.js
+++ js/newMeme.js
@@ -2,6 +2,7 @@ const fileUpload = document.getElementById('#fileUpload')
 let meme = null; 
 
 fileUpload.addEventListener('change', selectPic);
+btnUpload.addEventListener('click',uploadMeme);
 
 
 async function selectPic(){
@@ -13,7 +14,27 @@ async function selectPic(){
         const reader = new FileReader();
         reader.onload =(e) => {
             const newMeme = document.querySelector('newMeme');
-            newMeme.style.backgroundImage=`url`($'{e.trage}')
+            newMeme.style.backgroundImage=`url(${e.target.result}")`
         }
+    }
+}
+
+async function uploadMeme() {
+    try {
+        if (meme) {
+            const fromData=new fromData;
+            fromData.append('meme', meme);
+
+            const response = await fetch('http://127.0.0.1:3000/api/memes/upluadMeme',{
+                method: 'POST',
+                body: fromData,
+                credentials: 'include'
+            });
+        }else{
+            alert('Válassz ki egy képet!')
+        }
+        
+    } catch (error) {
+        console.log(error);
     }
 }
