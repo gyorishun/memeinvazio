6b7d7cfe35de77d8bf4d6fe1f8df198b9a8f3b33
tanulo <tanulo@barossgabor.local>
2025-05-26 13:36:03 +0200

    newmeme

--- js/home.js
+++ js/home.js
@@ -1,6 +1,14 @@
-const iconPlus = document.querySelector('fixed');
+const iconPlus = document.querySelector('.fixed');
 //console.log(iconPlus);
 
+window.addEventListener('DOMContentLoaded', getMemes);
 iconPlus.addEventListener('click', () => {
-    window.location.href='../html/newMeme.html';
+    window.location.href = '../html/newMeme.html';
 });
+
+async function getMemes() {
+    const response = await fetch('http://127.0.0.1:3000/api/memes/getMemes', {
+        method: 'GET',
+        credentials: 'include'
+    });
+}
--- js/newMeme.js
+++ js/newMeme.js
@@ -1,40 +1,52 @@
-const fileUpload = document.getElementById('#fileUpload')
-let meme = null; 
+// globális változók
+const fileUpload = document.querySelector('#fileUpload');
+const btnUpload = document.querySelector('.uploadButton');
+let meme = null;
+const iconHome = document.querySelector('.icon-home');
 
+// események
 fileUpload.addEventListener('change', selectPic);
-btnUpload.addEventListener('click',uploadMeme);
+btnUpload.addEventListener('click', uploadMeme);
+iconHome.addEventListener('click' , () => {
+    window.location.href = "../html/home.thml"
+});
 
-
-async function selectPic(){
-    const file = fileUpload.file[0];
+// függvények
+async function selectPic() {
+    const file = fileUpload.files[0];
     console.log(file);
-
+    
     if (file) {
         meme = file;
         const reader = new FileReader();
-        reader.onload =(e) => {
-            const newMeme = document.querySelector('newMeme');
-            newMeme.style.backgroundImage=`url(${e.target.result}")`
+        reader.onload = (e) => {
+            const newMeme = document.querySelector('.newMeme');
+            newMeme.style.backgroundImage = `url('${e.target.result}')`;
+            newMeme.style.backgroundRepeat = 'no-repeat';
         }
+        reader.readAsDataURL(file);
     }
 }
 
 async function uploadMeme() {
     try {
         if (meme) {
-            const fromData=new fromData;
-            fromData.append('meme', meme);
-
-            const response = await fetch('http://127.0.0.1:3000/api/memes/upluadMeme',{
+            const formData = new FormData;
+            formData.append('meme', meme);
+            //console.log(formData);
+            
+            const response = await fetch('http://127.0.0.1:3000/api/memes/uploadMeme', {
                 method: 'POST',
-                body: fromData,
+                body: formData,
                 credentials: 'include'
             });
-        }else{
-            alert('Válassz ki egy képet!')
+
+
+        } else {
+            alert('Válassz ki egy képet!');
         }
-        
     } catch (error) {
         console.log(error);
+        
     }
 }
